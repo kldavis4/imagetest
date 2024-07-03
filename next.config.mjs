740a765062c2503a4601f7d6920a0f8244de73f4
Kelly Davis <kldavis4@gmail.com>
2024-07-03 16:50:14 -0400

    foo

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        deviceSizes: [1024, 2048],
+        remotePatterns: [{
+            protocol: 'https',
+            hostname: 'api.armenia.travel',
+            pathname: 'storage/*'
+        },
+        {
+            protocol: 'https',
+            hostname: 'cdn.sanity.io',
+            pathname: 'images/*'
+        },
+        {
+            protocol: 'https',
+            hostname: 'www.campervannorway.com',
+            pathname: 'assets/*'
+        }]
+    }
+};
 
 export default nextConfig;
