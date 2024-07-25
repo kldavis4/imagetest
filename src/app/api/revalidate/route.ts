35cb593dc2d620174c043173a9a40b9ebc024b84
Kelly Davis <kldavis4@gmail.com>
2024-07-25 17:19:39 -0400

    add revalidate route

---
+++ src/app/api/revalidate/route.ts
@@ -0,0 +1,44 @@
+import { revalidatePath } from "next/cache";
+import { NextRequest, NextResponse } from "next/server";
+
+export const fetchCache = "force-no-store";
+
+const knownPaths = {
+  "/pages/homepage": "/",
+  robots: "/robots.txt",
+};
+
+const getRevalidationPath = (slug?: string | null) => {
+  if (!slug) {
+    throw new Error("Invalid slug");
+  }
+  if (knownPaths[slug]) {
+    return knownPaths[slug];
+  }
+
+  return slug;
+};
+
+export async function GET(request: NextRequest) {
+  try {
+    if (
+      request.nextUrl.searchParams.get("secret") !==
+      process.env.WP_PREVIEW_SECRET
+    ) {
+      return NextResponse.json({ message: "Invalid token" }, { status: 401 });
+    }
+
+    const slug = request.nextUrl.searchParams.get("slug");
+    const path = getRevalidationPath(slug);
+    revalidatePath(path);
+    const url = request.nextUrl.clone();
+    url.pathname = path;
+    url.searchParams.delete("slug");
+    url.searchParams.delete("secret");
+    return NextResponse.redirect(url, {
+      status: 307,
+    });
+  } catch (e) {
+    return NextResponse.json({ message: e.message }, { status: 401 });
+  }
+}
