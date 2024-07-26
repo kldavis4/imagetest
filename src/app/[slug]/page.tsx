62d5d3301ac85a8d1e97c531423bc8e31c19b87e
Kelly Davis <kldavis4@gmail.com>
2024-07-26 11:16:38 -0400

    add revalidate

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,3 +1,5 @@
+export const revalidate = 60
+
 export default async function Slug({ params }: { params: { slug: string } }) {
   const data = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/data/?slug=${params.slug}`).then((res) => res.json());
   return (
