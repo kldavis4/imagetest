1bc468c861c32310f9595f08389fe5437ad11260
Kelly Davis <kldavis4@gmail.com>
2024-07-26 11:21:01 -0400

    add generatestaticparams

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,5 +1,9 @@
 export const revalidate = 60
 
+export async function generateStaticParams() {
+  return [{ slug: "hello" }, { slug: "world" }, { slug: "foobar" }]
+}
+
 export default async function Slug({ params }: { params: { slug: string } }) {
   const data = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/data/?slug=${params.slug}`).then((res) => res.json());
   return (
