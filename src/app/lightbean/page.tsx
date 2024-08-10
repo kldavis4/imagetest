9274725ead3efb31cfa08b51338c8a85f1b9b790
Kelly Davis <kldavis4@gmail.com>
2024-08-09 21:17:23 -0400

    add revalidate

--- src/app/lightbean/page.tsx
+++ src/app/lightbean/page.tsx
@@ -1,3 +1,5 @@
+export const revalidate = 3;
+
 export default async function Lightbean({ params }: { params: {} }) {
     const data = await fetch(`http://api.lightbeans.com/organizations/banas-porcelain/products/tuscany-silver-36x24-v3`).then((res) => res.json());
     return (
