715d76ca9e191aa688404a9e2c6548f0b8bf8b88
Kelly Davis <kldavis4@gmail.com>
2024-08-09 21:30:38 -0400

    multiple requests

--- src/app/lightbean/page.tsx
+++ src/app/lightbean/page.tsx
@@ -1,11 +1,28 @@
 export const revalidate = 3;
-
+// TODO need source code
 export default async function Lightbean({ params }: { params: {} }) {
-    const data = await fetch(`http://api.lightbeans.com/organizations/banas-porcelain/products/tuscany-silver-36x24-v3`).then((res) => res.json());
+    const keys = [
+        'noce-48x24-v2',
+        'noce-24x24',
+        'tuscany-silver-36x24-v3',
+        'sahara-sands-36x24-v3',
+        'brown-timber-8x36-v3',
+        'sahara-sands-36x24-v3',
+        'morning-sky-heringbone',
+        'morning-sky-32x16-v3',
+        'grigio-48x24-v2',
+        'grigio-24x24',
+        'bianco-48x24-v2',
+        'bianco-24x24',
+    ]
+    const results = keys.map(async (key) => {
+        return await fetch(`http://api.lightbeans.com/organizations/banas-porcelain/products/${key}`).then((res) => res.json());
+    })
+
     return (
       <main>
         <div>
-          {JSON.stringify(data)}
+          {JSON.stringify(results)}
         </div>
       </main>
     );
