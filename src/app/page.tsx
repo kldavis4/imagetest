4116fb7daa77ebd5c5468f07a7b7b71a53b1cdaf
Kelly Davis <kldavis4@gmail.com>
2024-09-20 11:11:41 -0400

    add homepage tag to root

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
-export default function Home() {
+export default async function Home() {
   const images = []
-  for (let i = 0; i < 100; i++) {
+  for (let i = 0; i < 2; i++) {
     images.push({
       src: "https://cdn.sanity.io/images/y25r8d4i/production/19da7ddf12544b5f17203b084ed4be52f006974f-2048x1365.webp?rect=342,0,1365,1365&w=1000&h=1000&fit=crop&auto=format",
       alt: "Next.js Logo",
@@ -12,10 +12,17 @@ export default function Home() {
       quality: i
     })
   }
+  const res = await fetch(`http://api.lightbeans.com/organizations/banas-porcelain/products/noce-24x24`, {
+    next: {
+      tags: ['homepage'],
+      revalidate: 600
+    }
+  }).then((res) => res.json());
   return (
     <main className={styles.main}>
 
       <div className={styles.grid}>
+        <h1>{res}</h1>
         {images.map((image, index) => (
           <div key={index} className={styles.card}>
             <Image {...image} />
