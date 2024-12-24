f6858447f30abbbdbb088667fabaa360233e0d71
Kelly Davis <kldavis4@gmail.com>
2024-12-24 11:29:06 -0500

    change method

--- src/app/api/revalidateTags/route.ts
+++ src/app/api/revalidateTags/route.ts
@@ -2,8 +2,7 @@ import { revalidateTag } from "next/cache";
 import type { NextRequest  } from "next/server";
 import { NextResponse } from "next/server";
 
-
-export async function GET(request: NextRequest) {
+export async function POST(request: NextRequest) {
   try {
     const tags = request.nextUrl.searchParams.getAll("tag");
 
