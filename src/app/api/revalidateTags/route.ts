b7fe0d792e7bbe93a1288cac8c1198e8eb7e92ba
Kelly Davis <kldavis4@gmail.com>
2024-12-24 11:33:33 -0500

    allow get or post

--- src/app/api/revalidateTags/route.ts
+++ src/app/api/revalidateTags/route.ts
@@ -2,23 +2,31 @@ import { revalidateTag } from "next/cache";
 import type { NextRequest  } from "next/server";
 import { NextResponse } from "next/server";
 
-export async function POST(request: NextRequest) {
+async function handleRevalidate(request: NextRequest) {
   try {
     const tags = request.nextUrl.searchParams.getAll("tag");
 
     // console.log("Before revalidate: ", tags)
     // revalidateTag(tags.join(","));
     // console.log("After revalidate")
-  tags.filter((tag) => tag)
+    tags.filter((tag) => tag)
       .forEach((tag) => {
         console.info('Revalidating tag:', tag)
         revalidateTag(tag)
       })
 
     // return NextResponse.json({ message: "Revalidation started" });
-  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+    // biome-ignore lint/suspicious/noExplicitAny: <explanation>
     return new Response(null, { status: 200 });
   } catch (e: any) {
     return NextResponse.json({ message: e.message }, { status: 401 });
   }
+}
+
+export async function POST(request: NextRequest) {
+  return handleRevalidate(request);
+}
+
+export async function GET(request: NextRequest) {
+  return handleRevalidate(request);
 }
