0735729fba44c001d137e531e035a43398de26f2
Luba Kravchenko <luba.kravchenko@vercel.com>
2025-01-06 16:40:10 -0500

    new time

--- src/app/lightbean/page.tsx
+++ src/app/lightbean/page.tsx
@@ -29,7 +29,7 @@ export default async function Lightbean() {
     }
 
     const now = new Date();
-    if (now.getTime() < 1736195045000) {
+    if (now.getTime() < 1736206046000) {
       console.log(now.getTime())
       notFound();
     }
