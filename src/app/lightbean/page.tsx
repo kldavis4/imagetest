59c34d5a484fa5a562431754c35418f0beb4f5d4
Kelly Davis <kldavis4@gmail.com>
2025-01-15 13:44:30 -0500

    try 14.2.22

--- src/app/lightbean/page.tsx
+++ src/app/lightbean/page.tsx
@@ -29,7 +29,7 @@ export default async function Lightbean() {
     }
 
     const now = new Date();
-    if (now.getTime() < 1736958440000) {
+    if (now.getTime() < 1736966955000) {
       console.log(now.getTime())
       notFound();
     }
