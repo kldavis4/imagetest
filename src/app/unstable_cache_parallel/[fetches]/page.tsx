3fe16ea0bfbc14660b01848be721cfee74f066a5
Kelly Davis <kldavis4@gmail.com>
2025-02-28 15:14:32 -0500

    another try

--- src/app/unstable_cache_parallel/[fetches]/page.tsx
+++ src/app/unstable_cache_parallel/[fetches]/page.tsx
@@ -6,7 +6,7 @@ export const dynamic = 'force-dynamic'
 const fakeFetch = unstable_cache(async (id: string) => Promise.resolve({ id }), ['my-id']);
 
 interface PageProps {
-  params: { fetches: string }; // Dynamic route parameter
+  params: Promise<{ fetches: string }>; // Dynamic route parameter
 }
 
 const test = async () => {
@@ -17,7 +17,8 @@ const test = async () => {
 }
 
 const Page: NextPage<PageProps> = async ({ params }) => {
-  const fetches = parseInt(params.fetches || '1'); // Get fetches from params
+  const { fetches: fetchesParam } = await params; // Get fetches from params
+  const fetches = parseInt(fetchesParam || '1'); // Get fetches from params
 
   const durations = await Promise.all(Array(fetches).fill(0).map(async () => await test()));
   const duration = durations.join('ms, ');
