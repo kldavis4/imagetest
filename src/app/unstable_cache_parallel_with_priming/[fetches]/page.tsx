618dc28d679692846996ec8867541f44091e352f
Kelly Davis <kldavis4@gmail.com>
2025-02-28 15:23:52 -0500

    try priming the connection pool

---
+++ src/app/unstable_cache_parallel_with_priming/[fetches]/page.tsx
@@ -0,0 +1,40 @@
+import {NextPage} from "next";
+import { unstable_cache } from 'next/cache'
+
+export const dynamic = 'force-dynamic'
+
+const fakeFetch = unstable_cache(async (id: string) => Promise.resolve({ id }), ['my-id']);
+
+interface PageProps {
+  params: Promise<{ fetches: string }>; // Dynamic route parameter
+}
+
+const test = async () => {
+  const id = Math.random().toString(36).substring(7);
+  const start = Date.now();
+  const data = await fakeFetch(id);
+  return Date.now() - start;
+}
+
+const Page: NextPage<PageProps> = async ({ params }) => {
+  const { fetches: fetchesParam } = await params; // Get fetches from params
+  const fetches = parseInt(fetchesParam || '1'); // Get fetches from params
+
+  // initial query
+  for (let i = 0; i < fetches; i++) {
+    await test()
+  }
+
+  const durations = await Promise.all(Array(fetches).fill(0).map(async () => await test()));
+  const duration = durations.join('ms, ');
+
+  return (
+    <main>
+      <div>
+        {duration}
+      </div>
+    </main>
+  );
+}
+
+export default Page;
