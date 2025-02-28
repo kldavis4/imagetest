7bbdc78cca1667a555b971d8180708c867bbb6ab
Kelly Davis <kldavis4@gmail.com>
2025-02-28 17:20:57 -0500

    cleanup

--- src/app/unstable_cache_parallel_with_priming/[fetches]/page.tsx
+++ src/app/unstable_cache_parallel_with_priming/[fetches]/page.tsx
@@ -3,16 +3,17 @@ import { unstable_cache } from 'next/cache'
 
 export const dynamic = 'force-dynamic'
 
-const fakeFetch = unstable_cache(async (id: string) => Promise.resolve({ id }), ['my-id']);
 
 interface PageProps {
   params: Promise<{ fetches: string }>; // Dynamic route parameter
 }
 
+const fakeFetch = unstable_cache(async (id: string) => Promise.resolve({ id }), ['my-id']);
+
 const test = async () => {
   const id = Math.random().toString(36).substring(7);
   const start = Date.now();
-  const data = await fakeFetch(id);
+  await fakeFetch(id);
   return Date.now() - start;
 }
 
--- src/app/unstable_cache_sequential/[fetches]/page.tsx
+++ src/app/unstable_cache_sequential/[fetches]/page.tsx
@@ -3,23 +3,23 @@ import { unstable_cache } from 'next/cache'
 
 export const dynamic = 'force-dynamic'
 
-const fakeFetch = unstable_cache(async (id: string) => Promise.resolve({ id }), ['my-id']);
-
 interface PageProps {
   params: Promise<{ fetches: string }>; // Dynamic route parameter
 }
 
+const fakeFetch = unstable_cache(async (id: string) => Promise.resolve({ id }), ['my-id']);
+
 const test = async () => {
   const id = Math.random().toString(36).substring(7);
   const start = Date.now();
-  const data = await fakeFetch(id);
+  await fakeFetch(id);
   return Date.now() - start;
 }
 
 const Page: NextPage<PageProps> = async ({ params }) => {
   const { fetches: fetchesParam } = await params; // Get fetches from params
   const fetches = parseInt(fetchesParam || '1'); // Get fetches from params
-  
+
   // perform fetches sequentially
   const durations = [];
   for (let i = 0; i < fetches; i++) {
