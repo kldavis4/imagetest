57c6722b3302cc41f385f40a83a711ff9a79bc22
Kelly Davis <kldavis4@gmail.com>
2025-03-27 15:34:53 -0700

    demo middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,25 +1,36 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
 import { cacheGet, cacheSet } from './cache';
+import { NextRequest, NextResponse } from "next/server";
 
-export async function middleware(req: NextRequest) {
-  // Check if the request is for a static asset in the public folder
-  const { pathname } = req.nextUrl
-  console.log(pathname)
-  if (pathname.startsWith('/js')) {
-    const response = NextResponse.next()
-    // Add or modify headers
-    response.headers.set('ETag', 'CustomETagValue')
-    response.headers.set('X-Custom-Header', 'CustomHeaderValue')
-    return response
+export default async function middleware(request: NextRequest) {
+  if (request.nextUrl.pathname === "/middleware") {
+    return demo(request);
   }
-
-  const cacheKey = req.nextUrl.toString()
-  const cachedResponse = await cacheGet(cacheKey)
-  console.log({cachedResponse})
-  if (!cachedResponse) {
-    await cacheSet(cacheKey, 'Hello World')
-  }
-
-  return NextResponse.next()
 }
+
+async function demo(request: NextRequest) {
+  const key = request.nextUrl.searchParams.get("key") || "not-set";
+  const before = Date.now();
+  const fromCache = await cacheGet(key);
+  if (fromCache) {
+    return NextResponse.json({
+      cached: true,
+      readDuration: Date.now() - before,
+      key,
+      value: fromCache,
+    });
+  }
+  const value = new Date().toString();
+  const beforeWrite = Date.now();
+  await cacheSet(key, value);
+  return NextResponse.json({
+    cached: false,
+    readDuration: beforeWrite - before,
+    writeDuration: Date.now() - beforeWrite,
+    key,
+    value,
+  });
+}
+
+export const config = {
+  matcher: ["/middleware"],
+};
